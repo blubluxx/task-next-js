6299b6245a2e27afbafdb3e6730efa8eb7921ad9
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-02-22 22:01:16 +0200

    [FIX] Refactor stage description layout for improved responsiveness and aesthetics

--- src/components/stage/description.tsx
+++ src/components/stage/description.tsx
@@ -1,18 +1,18 @@
 const Description = () => {
   return (
-    <div className="relative md:p-12 p-2 py-7 md:bg-transparent bg-neutral-200">
-      <div className="relative md:bg-white bg-transparent md:bg-opacity-60 flex flex-col md:w-96 h-fit gap-10 justify-center items-center text-neutral-600 rounded-3xl p-5 font-semibold">
-        <h1 className="md:text-2xl text-4xl text-center font-bold md:p-0 px-8">
+    <div className="md:absolute md:top-1/3 md:left-1/3 md:transform md:-translate-x-2/3 md:-translate-y-1/2 md:w-1/3 md:max-h-min">
+      <div className="md:bg-white bg-neutral-200 md:bg-opacity-60 flex flex-col  md:gap-12 gap-8 justify-center items-center text-neutral-600 md:rounded-3xl p-8 font-semibold text-opacity-90 text-wrap ">
+        <h1 className="md:text-4xl text-3xl text-center font-bold md:p-0 px-8">
           ГРАБНИ НОВИ КЛИЕНТИ ЗА ТВОИТЕ УСЛУГИ.
         </h1>
-        <div className="text-wrap md:text-left text-center md:text-xs text-xl">
+        <div className="md:text-left text-center md:text-base text-lg">
           <p>Предоставяте услуги в бюти индустрията?</p>
           <p>
             Регистрирайте се бързо и лесно в платформата и вземете БЕЗПЛАТЕН
             достъп към всички предимства в рамките на първият месец
           </p>
         </div>
-        <button className="md:bg-blue-950 bg-yellow-400 md:shadow-sm shadow-[0px_2px_2px_rgba(0,0,0,0.3)] md:p-4 p-3 w-5/6 h-fit md:rounded-xl rounded-full md:text-sm text-l md:text-neutral-100 text-neutral-600">
+        <button className="md:bg-[#2f325b] bg-yellow-400 md:shadow-sm shadow-[0px_2px_2px_rgba(0,0,0,0.3)] md:p-4 p-3 md:w-2/3 w-[90%] h-fit md:rounded-xl rounded-full md:text-neutral-200 text-neutral-600">
           РЕГИСТРИРАЙТЕ СЕ
         </button>
       </div>
--- src/components/stage/stage-area.tsx
+++ src/components/stage/stage-area.tsx
@@ -4,17 +4,15 @@ import Description from "./description";
 const StageArea = () => {
   return (
     <section id="stage-area">
-      <div className="relative">
-        <div className="md:absolute relative">
-          <img
-            src="/bg-stage-area.webp"
-            alt="Stage Area Img"
-            className="md:w-screen w-fit min-h-full"
-          />
-        </div>
+      <div className="relative w-full">
+        <img
+          src="/bg-stage-area.webp"
+          alt="Stage Area Img"
+          className="w-screen"
+        />
         <Description />
+        <InfoPanel />
       </div>
-      <InfoPanel />
     </section>
   );
 };
