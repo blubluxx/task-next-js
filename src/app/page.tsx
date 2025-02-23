c49e892ba6de78a5a9f9a69a7953939f85727642
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-02-23 21:20:37 +0200

    [REFACTOR] Extract BenefitsPanel as a separate Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FirstStepsArea from "@/components/first_steps/first-steps-area";
 import HowItWorksArea from "@/components/how_it_works/how-it-works-area";
+import BenefitsArea from "@/components/benefits/benefits-panel";
 import RegisterArea from "@/components/register/register-area";
 import StageArea from "@/components/stage/stage-area";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <main>
         <StageArea />
         <FirstStepsArea />
+        <BenefitsArea />
         <RegisterArea />
         <HowItWorksArea />
       </main>
---
+++ src/components/benefits/benefits-panel.tsx
@@ -0,0 +1,21 @@
+import BenefitsList from "./benefits-list";
+
+const BenefitsArea = () => {
+  return (
+    <section id="benefits-area">
+      <div className="flex md:flex-row flex-col justify-center mt-8">
+        <BenefitsList />
+
+        <div className="bg-[#dddbce] text-center md:text-2xl text-xl text-neutral-600 p-20 flex-1 flex items-center">
+          <p className="italic font-semibold">
+            В личния си профил ще можете да проследявате получените оферти,
+            изпълнените поръчки, оценките и отзивите. <br /> Регистрирайте се
+            сега <br /> Вашият успех е само на няколко клика разстояние!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BenefitsArea;
