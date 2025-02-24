228e506a0d38f119053da3ef657cad0590c22198
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-02-24 21:11:10 +0200

    [FEAT] Adjust image size in InfoPanelItem for improved responsiveness

--- src/components/info_panel/info-panel-item.tsx
+++ src/components/info_panel/info-panel-item.tsx
@@ -9,7 +9,7 @@ const InfoPanelItem: React.FC<IPanelItem> = ({
   return (
     <li className="flex flex-row gap-2 md:p-4 p-2 text-wrap text-grey-500 flex-1 ">
       <div className="w-fit h-fit">
-        <img src={iconSrc} alt={iconAlt} className="w-28" />
+        <img src={iconSrc} alt={iconAlt} className="md:w-28 xs:w-20" />
       </div>
       <div>
         <h1 className="mb-4">{title}</h1>
