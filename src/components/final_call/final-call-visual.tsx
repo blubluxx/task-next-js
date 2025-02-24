b9c27ab9736712442d2d5edbe1624b974bccc498
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-02-25 01:10:18 +0200

    [FEAT] Extract visuals from FinalCallVisual component into a new Visuals1 component

--- src/components/final_call/final-call-visual.tsx
+++ src/components/final_call/final-call-visual.tsx
@@ -1,28 +1,9 @@
+import Visuals1 from "./visuals-1";
+
 const FinalCallVisual = () => {
   return (
     <div className="flex justify-center items-center relative">
-      <div className="relative flex justify-center items-center">
-        <img
-          src="/green-heart.webp"
-          alt="heart"
-          className="absolute bottom-[30vh] -right-[4vw] w-20 h-auto"
-        />
-        <img
-          src="/thinking.webp"
-          alt="easy-register"
-          className="absolute bottom-[58vh] -right-[5vw] w-auto h-24"
-        />
-        <img
-          src="/group.webp"
-          alt="group"
-          className="w-full h-auto max-w-xl object-contain"
-        />
-        <img
-          src="/yellow-heart.webp"
-          alt="heart"
-          className="absolute -top-[7vh] right-[24vw] w-24 h-auto"
-        />
-      </div>
+      <Visuals1 />
     </div>
   );
 };
---
+++ src/components/final_call/visuals-1.tsx
@@ -0,0 +1,28 @@
+const Visuals1 = () => {
+  return (
+    <div className="relative flex justify-center items-center">
+      <img
+        src="/yellow-heart.webp"
+        alt="heart"
+        className="absolute bottom-[54vh] left-[3vw] size-20"
+      />
+      <img
+        src="/green-heart.webp"
+        alt="heart"
+        className="absolute bottom-[30vh] -right-[4vw] size-20"
+      />
+      <img
+        src="/thinking.webp"
+        alt="easy-register"
+        className="absolute bottom-[60vh] -right-[7vw] w-auto h-24"
+      />
+      <img
+        src="/group.webp"
+        alt="group"
+        className="h-auto max-w-2xl min-w-xl"
+      />
+    </div>
+  );
+};
+
+export default Visuals1;
