e713fe1a3c6a8e16c4612c61a9106168681d59a4
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-02-24 16:07:04 +0200

    [FEAT] Refactor FinalCallArea and FinalCallVisual components for improved layout and responsiveness

--- src/components/final_call/final-call-area.tsx
+++ src/components/final_call/final-call-area.tsx
@@ -4,10 +4,12 @@ import FinalCallVisual from "./final-call-visual";
 const FinalCallArea = () => {
   return (
     <section id="final-call">
-      <div className="flex flex-row bg-gradient-to-tr from-[#dddbce] to-[#959389] justify-center items-center p-24 px-28">
-        <div className="flex flex-row relative gap-24">
-          <FinalCallPanel />
-          <FinalCallVisual />
+      <div className="flex flex-col justify-center items-center bg-transparent">
+        <div className="flex flex-row bg-gradient-to-tr from-[#dddbce] to-[#959389] justify-center items-center p-24 px-28 md:w-[98vw]">
+          <div className="flex flex-row relative gap-24">
+            <FinalCallPanel />
+            <FinalCallVisual />
+          </div>
         </div>
       </div>
     </section>
