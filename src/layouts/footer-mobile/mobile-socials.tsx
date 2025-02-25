37d28ab26f4b281b0b140f1915439dc3fc9be6cd
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-02-25 16:03:32 +0200

    [FEAT] Add MobileSocials component

---
+++ src/layouts/footer-mobile/mobile-socials.tsx
@@ -0,0 +1,28 @@
+interface Icon {
+  src: string;
+  alt: string;
+}
+
+const icons: Icon[] = [
+  { src: "/facebook-tiny.svg", alt: "Facebook icon by Icons8" },
+  { src: "/twitter-tiny.svg", alt: "Twitter Bird icon by Icons8" },
+  { src: "/instagram-tiny.svg", alt: "Instagram icon by Icons8" },
+];
+
+const MobileSocials = () => {
+  return (
+    <div className="flex flex-row h-full w-full justify-start gap-4">
+      {icons.map((icon: Icon, index) => (
+        <div key={index} className="h-full">
+          <img
+            src={icon.src}
+            alt={icon.alt}
+            style={{ width: "25px", height: "25px" }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MobileSocials;
